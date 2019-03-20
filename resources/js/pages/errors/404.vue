00668ccbd8f98229d36adae52d7583ed37b90d3b
Hans Schouten <account@emailo.nl>
2019-03-20 23:07:37 +0100

    404 Quasar styling added

--- resources/js/pages/errors/404.vue
+++ resources/js/pages/errors/404.vue
@@ -1,25 +1,39 @@
 <template>
-  <card class="text-center">
-    <h3 class="mb-4">{{ $t('page_not_found') }}</h3>
 
-    <div class="links">
-      <!-- Authenticated -->
-      <router-link v-if="user" :to="{ name: 'dashboard' }">
-        {{ $t('go_dashboard') }}
-      </router-link>
-      <!-- Guest -->
-      <template v-else>
-        <router-link :to="{ name: 'login' }">
-          {{ $t('login') }}
-        </router-link>
-      </template>
+  <div class="q-pa-md q-mt-xl">
+    <div class="row justify-center">
+      <div class="col-12" style="max-width: 500px;">
+        <q-card>
+          <q-banner inline-actions class="text-white bg-red">
+            {{ $t('page_not_found') }}
+          </q-banner>
+
+          <q-separator />
+
+          <div class="q-pa-lg">
+            <!-- Authenticated -->
+            <q-btn v-if="user" color="primary" :label="$t('go_dashboard')" :to="{ name: 'dashboard' }" />
+            <!-- Guest -->
+            <template v-else>
+              <q-btn color="primary" :label="$t('login')" :to="{ name: 'login' }" />
+            </template>
+          </div>
+        </q-card>
+      </div>
     </div>
-  </card>
+  </div>
+
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'NotFound',
-  layout: 'guest'
+  layout: 'guest',
+
+  computed: mapGetters({
+    user: 'auth/user'
+  }),
 }
 </script>
